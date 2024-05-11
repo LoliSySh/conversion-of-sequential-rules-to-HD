1fe568b182b8b47c65be639eeca3841f2c57854d
LoliSySh <156702881+LoliSySh@users.noreply.github.com>
2024-05-11 17:45:36 +0200

    Update Petrinetz.py

--- Petrinetz.py
+++ Petrinetz.py
@@ -3,13 +3,8 @@ import pm4py
 from pm4py.algo.discovery.inductive import factory as inductive_miner
 
 if __name__ == "__main__":
-    #log_path = os.path.join("tests", "compressed_input_data", "08_receipt.xes.gz")
-    #log = pm4py.read_xes(log_path)
-    #log = pm4py.read_xes(os.path.join("tests","compressed_input_data",r"C:\Users\Loli\BA\output.txt\BA\Datensätze\PermitLog.xes"))
-    log = pm4py.read_xes(os.path.join("tests","compressed_input_data",r"C:\Users\Loli\BA\output.txt\BA\Datensätze\BPI_Challenge_2019.xes"))
 
-    #heu_net = pm4py.discover_heuristics_net(log, dependency_threshold=0.99)
-   # pm4py.view_heuristics_net(heu_net)
+    log = pm4py.read_xes(os.path.join("tests","compressed_input_data",r"PATH TO DATASET\Dataset.xes"))
    
     #inductive miner 
     net, initial_marking, final_marking = pm4py.discover_petri_net_inductive(log)
@@ -24,5 +19,4 @@ if __name__ == "__main__":
     
     
     pm4py.view_petri_net(net, initial_marking, final_marking)
-   # pm4py.write_pnml(net, initial_marking, final_marking, "petriPermitLog_depthres99.pnml")
   
